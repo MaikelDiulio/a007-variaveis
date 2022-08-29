45ba43f384a5408f247a01a65014583d389edc0a
maikeldiulio <maikeltecba@gmail.com>
2022-08-29 14:25:30 -0300

    exercício 1 e 2 a007

---
+++ exercicio1/script.js
@@ -0,0 +1,3 @@
+let nome = prompt ("Qual oseu nome?")
+let idade = prompt ("Qual a sua idade?")
+console.log(nome, idade, typeof nome, idade)
---
+++ exercicio2/script.js
@@ -0,0 +1,3 @@
+let agua = confirm("vocè bebeu água hoje?")
+let treino = confirm("Já treinou hoje?")
+let musica = confirm("Já ouviu Música hoje?")
